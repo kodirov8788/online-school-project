03c6e7914911af6e00ed96734e7c1bbba9f43386
Klug007 <tilla.yosinbek@gmail.com>
2023-12-23 14:05:29 +0500

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/counter/counterSlice";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './pages/about/About';
-import Contact from './pages/contact/Contact';
-import Cources from './pages/cources/Cources';
-import Pricing from './pages/pricing/Pricing';
-import Signin from './pages/signin/Signin';
-import Signup from './pages/signup/Signup';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Cources from "./pages/cources/Cources";
+import Pricing from "./pages/pricing/Pricing";
+import Signin from "./pages/signin/Signin";
+import Signup from "./pages/signup/Signup";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/Cources' element={<Cources />} />
-        <Route path='/pricing' element={<Pricing />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
-
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/Cources" element={<Cources />} />
+                    <Route path="/pricing" element={<Pricing />} />
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
